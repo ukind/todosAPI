8e6c7a1013ddbddcbab4b68d3112c543d90b0985
ukind <ukind@users.noreply.github.com>
2018-04-09 13:12:40 +0700

    move to routes helpers

--- routes/todos.js
+++ routes/todos.js
@@ -1,69 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const helpers = require('../helpers/todos');
 
-// INDEX ROUTES
-router.get('/', function(req, res) {
-  db.TodoCollection.find()
-  .then(todos => {
-    res.json(todos);
-  })
-  .catch(error => {
-    res.send(error);
-  });
-});
-
-// CREATE ROUTES
-router.post('/', function(req, res) {
-  db.TodoCollection.create(req.body)
-  .then(newTodo => {
-    res.json(newTodo);
-  })
-  .catch(error => {
-    res.send(error);
-  });
-});
+router.route('/')
+.get(helpers.getTodoAll)
+.post(helpers.postTodo);
 
 // SHOW ROUTES
-router.get('/:todoID', (req, res) => {
-  db.TodoCollection.findById(req.params.todoID)
-  .then(foundID => {
-    res.json(foundID);
-  })
-  .catch(err => {
-    res.send(err);
-  });
-});
-
-// UPDATES ROUTE
-router.put('/:todoID', (req, res) => {
-  const todoID = req.params.todoID;
-  db.TodoCollection.findOneAndUpdate(
-    {_id: todoID},
-    req.body,
-  {new: true})
-  .then(changedTodo => {
-    res.json(changedTodo);
-  })
-  .catch(err => {
-    res.send(err);
-  });
-});
-
-// DELETES ROUTES
-router.delete('/:todoID', (req, res) => {
-  const ID = req.params.todoID;
-  db.TodoCollection.remove({
-    _id: ID
-  })
-  .then(deletedID => {
-    res.json({
-      message: 'data deleted'
-    });
-  })
-  .catch(err => {
-    res.send(err);
-  });
-});
+router.route('/:todoID')
+.get(helpers.getTodosID)
+.put(helpers.putTodo)
+.delete(helpers.deleteToDo);
 
 module.exports = router;
